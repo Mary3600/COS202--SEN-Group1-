4ffb9d00ba3268cd7ef8e476c32feff05db4838c
Kendara Ntekop <kendarantekop@gmail.com>
2026-05-02 01:29:26 -0700

    refactor: Split sidebar UI from calendar UI into components folder

--- frontend/app/calendar/page.tsx
+++ frontend/app/calendar/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import Sidebar from "@/components/Sidebar";
 
 import Image from "next/image";
 import { Bell, Settings, ChevronLeft, ChevronRight, Search, LayoutDashboard, CalendarDays, ArchiveRestore, Menu, Bold  } from "lucide-react";
@@ -47,123 +48,14 @@ const [collapsed, setCollapsed] = useState(false);
   return (
     <div style={{ display: "flex", minHeight: "100vh" }}>
 
-   {/* 🟦 SIDEBAR */}
-{sidebarOpen && (
-  <div
-    style={{
-      width: collapsed ? "70px" : "280px",
-      transition: "0.3s",
-      backgroundColor: "#F1F5F9",
-      padding: "20px",
-      color: "#fff",
-      display: "flex",
-      flexDirection: "column",
-      gap: "20px",
-    }}
-  >
-    {/* TOP (Logo + toggle) */}
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: collapsed ? "center" : "space-between",
-        padding: "10px 0px 20px 0px",
-      }}
-    >
-      {!collapsed && (
-      <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
-        <Image src="/Momento.jpeg" alt="Logo" width={50} height={50} />
-        <div>
-          <p style={{ fontWeight: "bold", color: "#165A50" }}>Momento</p>
-          <p style={{ fontSize: "11px", paddingTop: "3px", color : "#64748B" }}>THE MINDFUL SCHOLAR</p>
-        </div>
-      </div>
-    )}
-
-      <Menu
-        size={22}
-        style={{ cursor: "pointer", color: "#165A50" }}
-        onClick={() => setCollapsed(!collapsed)}
-      />
-    </div>
-
-    {/* LOGO */}
-    
-
-    {/* NAV ITEMS */}
-    <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
-      
-      {/* DASHBOARD */}
-      <div
-        onClick={() => setActivePage("dashboard")}
-        style={{
-          display: "flex",
-          alignItems: "center",
-          gap: "10px",
-          justifyContent: collapsed ? "center" : "flex-start",
-          cursor: "pointer",
-          color: activePage === "dashboard" ? "#165A50" : "#64748B",
-          fontWeight: activePage == "dashboard" ? "bold" : "normal",
-        }}
-      >
-        <LayoutDashboard size={20}    />
-        {!collapsed && <span>Dashboard</span>}
-      </div>
-
-      {/* CALENDAR */}
-      <div
-        onClick={() => setActivePage("calendar")}
-        style={{
-          display: "flex",
-          alignItems: "center",
-          gap: "10px",
-          justifyContent: collapsed ? "center" : "flex-start",
-          cursor: "pointer",
-          color: activePage === "calendar" ? "#165A50" : "#64748B",
-          fontWeight: activePage == "calendar" ? "bold" : "normal",
-        }}
-      >
-        <CalendarDays size={20} />
-        {!collapsed && <span>Calendar</span>}
-      </div>
-
-      {/* ARCHIVE */}
-      <div
-        onClick={() => setActivePage("archive")}
-        style={{
-          display: "flex",
-          alignItems: "center",
-          gap: "10px",
-          justifyContent: collapsed ? "center" : "flex-start",
-          cursor: "pointer",
-          color: activePage === "archive" ? "#165A50" : "#64748B",
-          fontWeight: activePage == "archive" ? "bold" : "normal",
-        }}
-      >
-        <ArchiveRestore size={20} />
-        {!collapsed && <span>Archive</span>}
-      </div>
-    </div>
-
-    {/* ADD TASK BUTTON */}
-    <div style={{ marginTop: "auto" }}>
-      <button
-        style={{
-          width: "100%",
-          padding: "10px",
-          borderRadius: "8px",
-          border: "20px",
-          backgroundColor: "#165A50",
-          color: "#fff",
-          fontWeight: "bold",
-          cursor: "pointer",
-        }}
-      >
-        {collapsed ? "+" : "+ Add Task"}
-      </button>
-    </div>
-  </div>
-)}
+   <Sidebar
+  sidebarOpen={sidebarOpen}
+  setSidebarOpen={setSidebarOpen}
+  collapsed={collapsed}
+  setCollapsed={setCollapsed}
+  activePage={activePage}
+  setActivePage={setActivePage}
+/>
 
       {/* 🟩 MAIN CONTENT */}
       <div
@@ -171,8 +63,7 @@ const [collapsed, setCollapsed] = useState(false);
           flex: 1,
           padding: "30px",
           backgroundColor: "#ffffff",
-          filter: sidebarOpen && !collapsed ? "blur(2px)" : "none",
-          transition: "0.3s",
+          
         }}
       >
         {/* OPEN SIDEBAR BUTTON */}
